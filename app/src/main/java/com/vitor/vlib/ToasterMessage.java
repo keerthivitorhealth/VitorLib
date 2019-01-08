f4cff259c686e6438441db092f79a468e3e5295a
keerthivitorhealth <45257922+keerthivitorhealth@users.noreply.github.com>
2019-01-08 15:17:35 +0530

    Spirometer Main app Toast msg

---
+++ app/src/main/java/com/vitor/vlib/ToasterMessage.java
@@ -0,0 +1,11 @@
+package com.vitor.vlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void toast(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
